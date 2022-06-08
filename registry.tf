4205ff276313ca0a058d3b1b79acf91c955aaa85
Ardhi Ramadhani <ardhidhani@gmail.com>
2022-06-08 19:11:43 +0700

    enable the Artifact Registry API

--- registry.tf
+++ registry.tf
@@ -1,3 +1,13 @@
+resource "google_project_service" "service" {
+ for_each = toset([
+  "artifactregistry.googleapis.com"
+ ])
+ service = each.key
+ 
+ project = var.project
+ disable_on_destroy = false
+}
+
 resource "google_artifact_registry_repository" "backend-foedtra" {
  provider = google-beta
  location = var.region
