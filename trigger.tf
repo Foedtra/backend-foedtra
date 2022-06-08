0ebaeebad1ddaf87473f1a70da017ca9abc84c38
Ardhi Ramadhani <ardhidhani@gmail.com>
2022-06-09 06:52:06 +0700

    add push container image to container registry and deploy container image to cloud run

--- trigger.tf
+++ trigger.tf
@@ -1,3 +1,42 @@
+data "google_project" "project" {
+    project_id = var.project
+}
+
+resource "google_project_service" "service" {
+    for_each = toset([
+        "artifactregistry.googleapis.com",
+        "run.googleapis.com"
+    ])
+
+    service = each.key
+
+    project            = var.project
+    disable_on_destroy = false
+}
+
+resource "google_project_iam_member" "cloudbuild-run" {
+    project = var.project
+    role    = "roles/run.admin"
+    member  = "serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
+}
+
+resource "google_project_iam_member" "cloudbuild-iam" {
+    project = var.project
+    role    = "roles/iam.serviceAccountUser"
+    member  = "serviceAccount:${data.google_project.project.number}@cloudbuild.gserviceaccount.com"
+}
+
+resource "google_artifact_registry_repository" "backend-foedtra" {
+    provider = google-beta
+    location = var.region
+    repository_id = "backend-foedtra"
+    format = "DOCKER"
+
+    depends_on = [
+        google_project_service.service["artifactregistry.googleapis.com"]
+    ]
+}
+
 resource "google_cloudbuild_trigger" "my-trigger" {
   provider = google-beta
   name = "backend-foedtra"
@@ -13,6 +52,8 @@ resource "google_cloudbuild_trigger" "my-trigger" {
   substitutions = {
     _REGISTRY = google_artifact_registry_repository.backend-foedtra.repository_id
     _REGISTRY_URL = "${var.region}-docker.pkg.dev"
+    _REGION = var.region
+    _PROJECT_NAME = var.project
   }
 
   filename = "cloudbuild.yaml"
